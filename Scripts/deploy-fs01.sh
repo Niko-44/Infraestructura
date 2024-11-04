a40146577a72cc88d5463d8483880cf4079ea0b2
Niko-44 <106493627+Niko-44@users.noreply.github.com>
2024-11-04 13:22:38 -0300

    Add files via upload

--- Scripts/deploy-fs01.sh
+++ Scripts/deploy-fs01.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-apt update -y && apt upgrade -y
 
-apt-get install acl attr samba samba-client winbind libpam-winbind libnss-winbind dnsutils python3-setproctitle -y
+apt update -y
+apt upgrade -y
+
+apt install acl attr samba samba-client winbind libpam-winbind libnss-winbind dnsutils python3-setproctitle -y
 
 DOMAIN="NF-TIPY-UY"
 REALM="${DOMAIN}.DUCKDNS.ORG"
-ADMINPASS="Passw0rd"
+ADMINPASSWORD="Passw0rd"
 
-cat << EOF> /etc/krb5.conf
-
+cat << EOF > /etc/krb5.conf
 [libdefaults]
     default_realm = ${REALM}
     dns_lookup_realm = false
-    dns_lookup_kdc = false
-
+    dns_lookup_kdc = true
 EOF
 
-cat << EOF> /etc/samba/smb.conf
+cat << EOF > /etc/samba/smb.conf
 
 security = ADS
 workgroup = ${DOMAIN}
@@ -28,10 +28,10 @@ log level = 1
 idmap config * : backend = tdb
 idmap config * : range = 3000-7999
 
-idmap config ${DOMAIN}:backend = ad
-idmap config ${DOMAIN}:schema_mode = rfc2307
-idmap config ${DOMAIN}:range = 10000-999999
-idmap config ${DOMAIN}:unix_nss_info = yes
+idmap config ${DOMAIN} : backend = ad
+idmap config ${DOMAIN} : schema_mode = rfc2307
+idmap config ${DOMAIN} : range = 10000-999999
+idmap config ${DOMAIN} : unix_nss_info = yes
 
 vfs objects = acl_xattr
 map acl inherit = yes
@@ -40,49 +40,43 @@ store dos attributes = yes
 winbind enum users = yes
 winbind enum groups = yes
 
-[Public]
-    path = /srv/fileserver/public/
-    read only = no
+[Publico]
+path = /srv/fileserver/publico/
+read only = no
 
-[Private]
-    path = /srv/fileserver/private/
-    read only = no
+[Instaladores]
+path = /srv/fileserver/instaladores/
+read only = no
+
 
 EOF
 
-net ads join -U administrator%${ADMINPASS}
+net ads join -U administrator%$ADMINPASSWORD
 
-cat << EOF> /etc/nsswitch.conf
-
-passwd: files systemd winbind
-group: files systemd winbind
+cat << EOF > /etc/nsswitch.conf
+passwd: files winbind
+group: files winbind
 shadow: files systemd
 gshadow: files systemd
-
 hosts: files dns
 networks: files
-
 protocols: db files
 services: db files
 ethers: db files
 rpc: db files
-
-netgroups: nis
-
+netgroup: nis
 EOF
 
 systemctl restart winbind
 
 
-#getent passwd
+mkdir -p /srv/fileserver/publico/
+mkdir -p /srv/fileserver/instaladores/
 
-mkdir -p /srv/fileserver/public/
-mkdir -p /srv/fileserver/private/
+chown root:"NF-TIPY-UY\domain users" /srv/fileserver/publico/
+chown root:"NF-TIPY-UY\sysadmins" /srv/fileserver/instaladores/
 
-chown root:"NF-TIPY-UY\domain users" /srv/fileserver/public/
-chown "NF-TIPY-UY\nicolas.ferreira":root /srv/fileserver/private/
-
-chmod 0750 /srv/fileserver/public/
-chmod 0770 /srv/fileserver/private/
+chmod 0750 /srv/fileserver/publico/
+chmod 0770 /srv/fileserver/instaladores/
 
 systemctl restart smbd
